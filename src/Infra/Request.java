47ad762bed384ee82c95f6875dc7bb1128646fd8
Justin707314 <Justin707314@gmail.com>
2022-08-22 17:17:27 +0900

    uriPaser와 Request 구현

---
+++ src/Infra/Request.java
@@ -0,0 +1,34 @@
+package Infra;
+
+import Utils.UriParser;
+
+import java.util.Map;
+
+public class Request {
+
+    private UriParser uriParser;
+
+    private Request(String uri){
+        this.uriParser = new UriParser(uri);
+    }
+
+    public boolean isValidRequest(){
+        return false;
+    }
+
+    public String getControllerCode(){
+        return uriParser.getControllerCode();
+    }
+
+    public String getTarget(){
+        return uriParser.getTarget();
+    }
+
+    public Object getParameter(String key, Class cls){
+        Map<String, Object> parameter = uriParser.getParameter();
+        return cls.cast(parameter.get(key));
+    }
+
+    
+
+}
